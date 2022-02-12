3e1df2ceee3ae0005531ab7bbbcc7259d99b69fb
Maksimova Kristel <idmaksimovak@gmail.com>
2022-02-12 21:45:27 +0300

    Initial Commit

---
+++ AwardsCollectionApp/AwardsCollectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsCollectionAppApp.swift
+//  AwardsCollectionApp
+//
+//  Created by Kristi on 12.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct AwardsCollectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AwardsCollectionApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AwardsCollectionApp
+//
+//  Created by Kristi on 12.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
